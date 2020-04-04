8a50c770a421da95640138548f35facb490fb140
Garrett <guitarguy13@ymail.com>
2020-04-04 13:28:14 -0500

    Double checked game

--- guessing_game.py
+++ guessing_game.py
@@ -14,7 +14,7 @@ def start_game():
     print('-----------------------------------')
     print('Welcome To The Number Guessing Game')
     print('-----------------------------------')
-    print('Current Highscore = ' + str(high_score))
+    print('Current Highscore is ' + str(high_score))
     secret_numer = (round(random.random() * 100))
     attempts = 1
     user_guess = input('Guess a number between 1 and 100: ')
@@ -22,12 +22,13 @@ def start_game():
 
     while keep_looping:
         try:
+            user_guess = int(user_guess)
             if int(user_guess) < 0 or int(user_guess) > 100:
                 user_guess = input(
                     'Error - Please guess a number between 0 and 100: ')
             else:
                 if int(user_guess) == secret_numer:
-                    print("You guessed the correct number! It was {} and it took you {} tries".format(
+                    print("You guessed the correct number! It was {}. It took you {} tries.".format(
                         secret_numer, attempts))
                     if attempts < high_score:
                         print('You set a new highscore! Congrats')
@@ -46,7 +47,8 @@ def start_game():
                     user_guess = int(input("It's lower. Guess again: "))
                     attempts += 1
         except ValueError:
-            user_guess = int(input('Error - Please enter a number: '))
+            user_guess = input('Error - Please enter a number: ')
+            continue
 
 
 if __name__ == '__main__':
